5525df1171630d14fb7ecb94fe9fc1ece369884f
Bruce Wolfeld <bwolfeld@users.noreply.github.com>
2018-04-28 09:29:10 -0400

    xz
    zxc

--- Stats-Bruce.R
+++ Stats-Bruce.R
@@ -4,6 +4,8 @@ library(readr)
 library(gmodels)
 library(MLmetrics)
 source("/Users/wolfeb3/Dropbox/Babson/QTM6300, Data Explorations and Analytics/BabsonAnalytics.R")
+set.seed(1234)
+
 
 #lookup table for converting total order value to int
 keys <- c("unknown","0 - 20","20 - 40", "40 - 60","60 - 80","80 - 100","100 - 150","150 - 200","200 - 250" ,"250 - 300","300 - 400","400 - 500","500 - 600","600 - 700","700 +")
@@ -16,39 +18,49 @@ df = read_csv("C:/Users/wolfeb3/desktop/wayfair_click_stream_short.csv")
 #Convert data
 df$testgroupname <- as.factor(df$testgroupname)
 df$testgroupname <- as.logical(df$testgroupname)
-df$testgroupname <- as.factor(df$testgroupname)
 df$HashSKU = NULL
 df$PriceBucket <- dflookup[match(df$PriceBucket,dflookup$keys, nomatch=-1),2]
 df$isshipsintime <- as.factor(df$isshipsintime)
-#df$opid <- 
+df$opid <- NULL
 df$SessionCount <- as.factor(df$SessionCount)
+df$lost <- (df$HadReceiptPage==FALSE & df$HadBasket==TRUE)
+sum(df$lost)
+
+df$testgroupname <- NULL
+df$HadReceiptPage <- NULL
+df$HadBasket<- NULL
+df$VisitorType <- as.factor(df$VisitorType)
+df$MkcName <- as.factor(df$MkcName)
+df$PriceBucket <- as.factor((df$PriceBucket))
+df$Platform <- as.factor((df$Platform))
+
+
+#logistic model
+#Part 2 - Partition the data using a 60-40 training-test split, and construct Na¨ive
+#Bayes' predictions for the target
+N = nrow(df)
+trainingSize = round(N*0.6)
+trainingCases = sample(N, trainingSize)
+training = df[trainingCases,]
+test = df[-trainingCases,]
+
+model = glm( df$lost~ ., data = training, family = binomial)
+pred = predict(model, test, type="class")
 
 
 
-tracebackdf$Customer_ID = NULL
-#df$Customer_Session_Start_Date  <- as. (df$Custom_Session_Start_Date) 
-df$Order_ID = NULL
-df$Order_Product_ID = NULL
-df$Product_ID = NULL
-#df$Purchased_Qty
-#df$Returned_Qty
-df$Cancelled <- as.logical(df$Cancelled == 1)
-df$Guarantee_Shown <- as.logical(df$Guarantee_Shown == 1)
-df$Product_Category <- as.factor(df$Product_Category)
-#df$Total_Order_Value
-#df$Customer_Estimated_Delivery_Date = NULL
-df$Customer_Actual_Delivery_Date = NULL
-df$visitor_type_Id <- as.factor(df$Visitor_Type_ID)
-df$Vistor_Type_Name = NULL
-df$Platform_ID <- as.logical(df$Platform_ID == 2)
-df$Platform_Name = NULL
-df$Customer_Session_Start_Date = NULL
-df$ShipClassName = NULL
+#Segment Customer Type
+totalDollarsPDP <- sum(df$PriceBucket*df$HadPDP)
+sum(df$HadPDP)
+totalDollarsPDP
 
-# Some Numbers
-#Order Average
-OrderAverage = mean(df$Total_Order_Value)
+totalDollarsBasket <- sum(df$PriceBucket*df$HadBasket)
+sum(df$HadBasket)
+totalDollarsBasket
 
+totalDollarsReceipt <- sum(df$PriceBucket*df$HadReceiptPage)
+sum(df$HadReceiptPage)
+totalDollarsReceipt
 
-
-
+DollarsLost <- totalDollarsBasket - totalDollarsReceipt
+DollarsLost
