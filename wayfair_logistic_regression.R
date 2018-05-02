455b6c6f468d5f862792dc57021f901e12c5f635
ianmboyd <iboyd@babson.edu>
2018-05-01 20:54:23 -0400

    Update - after team work

--- wayfair_logistic_regression.R
+++ wayfair_logistic_regression.R
@@ -17,6 +17,53 @@ if( IAN ){
   df = read_csv("C:/Users/wolfeb3/desktop/wayfair_click_stream_short.csv")
 }
 
+
+#
+# Format Data
+#
+#
+df$HadBasket = as.factor(df$HadBasket)
+df$HadBasket = as.logical(df$HadBasket)
+#df = df[df$HadBasket == 1, ]
+
+df$HadBasket = as.factor(df$HadBasket)
+df$HadBasket = as.logical(df$HadBasket)
+df$HadReceiptPage = as.factor(df$HadReceiptPage)
+df$HadReceiptPage = as.logical(df$HadReceiptPage)
+
+df$testgroupname = as.factor(df$testgroupname)
+df$Platform = as.factor(df$Platform)
+df$VisitorType = as.factor(df$VisitorType)
+
+# Make NULL values "other"
+df$MkcName[is.na(df$MkcName)] = '(Other)'
+# df$MkcName['(Other)'] = "AA_OTHER";
+
+df$MkcName = as.factor(df$MkcName)
+df$PriceBucket = as.factor(df$PriceBucket)
+
+df$HadPDP = as.factor(df$HadPDP)
+df$HadPDP = as.logical(df$HadPDP)
+
+df$isshipsintime = as.logical(df$isshipsintime)
+
+
+
+#
+# Create derived target (abandoned cart)
+#
+
+df$drop_cart <- ( df$HadReceiptPage == FALSE & df$HadBasket == TRUE)
+
+#
+# Inspect this data a bit and check for integrity
+# 
+
+convert_rate = sum(df$HadReceiptPage) / nrow(df); # % of people who checked out - 0.07426
+abandon_rate = sum(df$drop_cart) / sum(df$HadBasket); # % of people who had a basket that abandoned it - 0.7402201
+
+
+
 # Clean data
 
 #Remove un-needed columns
@@ -27,25 +74,6 @@ df$opid = NULL;
 df$HashSKU = NULL;
 
 
-df$HadBasket = as.factor(df$HadBasket)
-df$HadBasket = as.logical(df$HadBasket)
-df$HadReceiptPage = as.factor(df$HadReceiptPage)
-df$HadReceiptPage = as.logical(df$HadReceiptPage)
-
-df$testgroupname = as.factor(df$testgroupname)
-df$Platform = as.factor(df$Platform)
-df$VisitorType = as.factor(df$VisitorType)
-df$MkcName = as.factor(df$MkcName)
-df$PriceBucket = as.factor(df$PriceBucket)
-
-df$HadPDP = as.factor(df$HadPDP)
-df$HadPDP = as.logical(df$HadPDP)
-
-df$isshipsintime = as.logical(df$isshipsintime)
-
-# Create derived target (abandoned cart)
-
-df$drop_cart <- ( df$HadReceiptPage == FALSE & df$HadBasket == TRUE)
 
 # Remove variables used to derive target
 df$HadBasket = NULL
@@ -63,13 +91,34 @@ test = df[-trainingCases, ]
 # Create the Model 
 model = glm(drop_cart ~ ., data=train, family=binomial) # Generalized Linear Model
 # Step the Model Down
-step_model = step(model)
+#step_model = step(model) # Step did not remove any variables
 
 # Run some predictions...
 # 
 pred = predict(model, test, type="response")
-step_pred = predict(step_model, test, type="response")
 
-predTF = pred >.5
-step_predTF = step_pred  >.5
+predTF = pred >.25
 
+errorRate = sum(predTF != test$drop_cart)/nrow(test)
+errorBench = benchmarkErrorRate(train$drop_cart, test$drop_cart)
+
+
+CrossTable(predTF, test$drop_cart, expected = F, prop.r = F, prop.c = F, prop.t = F, prop.chisq = F)
+#              | test$drop_cart 
+#       predTF |     FALSE |      TRUE | Row Total | 
+# -------------|-----------|-----------|-----------|
+#        FALSE |     32004 |      3940 |     35944 | 
+# -------------|-----------|-----------|-----------|
+#         TRUE |       478 |      3578 |      4056 | 
+# -------------|-----------|-----------|-----------|
+# Column Total |     32482 |      7518 |     40000 | 
+# -------------|-----------|-----------|-----------|
+# 
+
+# 
+# False Negative Rate : FN/(TP+FN) : 1-Sensitivity 
+fnr = 3940/(3578+3940) # 52%
+
+# 
+# False Postive Rate :  FP/(FP+TN) : 1-Specificity 
+fpr = 478/(478+32004) # 2%
